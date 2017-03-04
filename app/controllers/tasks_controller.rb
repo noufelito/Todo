91a96e6e5ee54b3ccca9ea53d310d471399175d9
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-03-04 16:54:20 +0100

    set_task method added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
+	before_action :set_task, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@tasks = Task.all
 	end
 
 	def show
-		@task = Task.find(params[:id])
+		
 	end
 
 	def new
@@ -20,17 +21,14 @@ class TasksController < ApplicationController
 	end
 
 	def edit
-		@task = Task.find(params[:id])
 	end
 
 	def update
-		@task = Task.find(params[:id])
 		@task.update(task_params)
 		redirect_to @task
 	end
 
 	def destroy
-		@task = Task.find(params[:id])
 		@task.destroy
 		redirect_to tasks_path
 	end
@@ -38,5 +36,9 @@ class TasksController < ApplicationController
 	private
 		def task_params
 			params.require(:task).permit(:title, :body)
+		end
+
+		def set_task
+			@task = Task.find(params[:id])
 		end
 end
