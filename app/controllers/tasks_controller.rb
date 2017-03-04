d5daf645458190067ed6e348e9d4ef8b610a0430
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-03-04 16:32:42 +0100

    adding edit option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,16 @@ class TasksController < ApplicationController
 		redirect_to @task
 	end
 
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+		redirect_to @task
+	end
+
 	private
 		def task_params
 			params.require(:task).permit(:title, :body)
